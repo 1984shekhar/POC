029b396fd96e02c0ddd3aaecf6e4d8e5c10a8ecc
cs pandey <chandrashekharpandey@hotmail.com>
2023-01-29 13:25:18 +0530

    removed package

--- SSLPokeConnectivity/src/ServerSocketExample.java
+++ SSLPokeConnectivity/src/ServerSocketExample.java
@@ -1,4 +1,4 @@
-package com.mycompany;
+
 
 import java.io.IOException;
 import java.io.InputStream;
--- SSLPokeConnectivity/src/SslPokeConnectivity.java
+++ SSLPokeConnectivity/src/SslPokeConnectivity.java
@@ -1,4 +1,4 @@
-package com.mycompany;
+
 
 import java.io.InputStream;
 import java.io.OutputStream;
